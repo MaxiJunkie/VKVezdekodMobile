629354ff69bd35da551833c62c6427e58359f48a
MaxStegnienko <m.stegnienko@city-mobil.ru>
2020-09-11 15:20:25 +0300

    Initial Commit

---
+++ VKDonations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKDonations
+//
+//  Created by Максим Стегниенко on 11.09.2020.
+//  Copyright © 2020 Максим Стегниенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
